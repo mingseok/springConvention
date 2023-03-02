e4420433f31d18f767888257638a2a845b8bad4d
minseok <alstjr706@naver.com>
2023-03-02 22:04:44 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springconvention/SpringConventionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springconvention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConventionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConventionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springconvention/SpringConventionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconvention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConventionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
